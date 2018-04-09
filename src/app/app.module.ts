399d4d9be8b7192455c121ef4e5d7bd16109213f
krishna.chandra <Krishna.Chandra4@T-Mobile.com>
2018-04-09 13:50:32 -0700

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,42 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
-
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BASE_URL } from './app.tokens';
+import { SharedModule } from './shared/shared.module';
+import { AppRouterModule } from './app.routes';
+import { HomeComponent } from './home/home.component';
+import { FlightHistoryComponent } from './flight-history/flight-history.component';
+import { BookingsComponent } from './bookings/bookings.component';
+import { FlightBookingModule } from './flight-booking/flight-booking.module';
+import { ChatComponent } from './flight-booking/chat/chat.component';
+import {RouteReuseStrategy} from '@angular/router';
+import {CustomReuseStrategy} from './shared/router/custom-reuse-strategy';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    FormsModule,
+    ReactiveFormsModule,
+    SharedModule.forRoot(),
+    AppRouterModule,
+    //FlightBookingModule // <-- Würde Lazy Loading verhindern
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    BookingsComponent,
+    FlightHistoryComponent
+  ],
+  providers: [
+    {provide: BASE_URL, useValue: 'http://www.angular.at'},
+    {provide: RouteReuseStrategy, useClass: CustomReuseStrategy}
+  ],
+  bootstrap: [
+    AppComponent
+  ]
 })
-export class AppModule { }
+export class AppModule {
+}
